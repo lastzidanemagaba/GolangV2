11c058f49fc42f84d46189556056ad60555b3fdb
ZidaneMagaba <lastzidanemagaba@gmail.com>
2022-06-21 19:48:25 +0700

    gass

--- app/router.go
+++ app/router.go
@@ -11,8 +11,8 @@ func route() {
 	router.Use(static.Serve("/", static.LocalFile("./web", true))) //for the vue app
 
 	router.GET("/", controller.Index)
-	router.POST("/user", controller.CreateUser)
-	router.POST("/todo", middlewares.TokenAuthMiddleware(), controller.CreateTodo)
 	router.POST("/auth/login", controller.Login)
+	router.POST("/auth/register", controller.CreateUser)
 	router.POST("/auth/logout", middlewares.TokenAuthMiddleware(), controller.LogOut)
+	router.POST("/todo", middlewares.TokenAuthMiddleware(), controller.CreateTodo)
 }
